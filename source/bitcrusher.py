259fa5eacdf6c9390ab7d3494a01ff960b699b4a
yves-obreykov <yves.obreykov@gmail.com>
2025-10-16 10:49:53 +0200

    Created the program

---
+++ source/bitcrusher.py
@@ -0,0 +1,42 @@
+# bitcrusher program written by Yves
+
+import soundfile as sf
+import numpy as np
+import argparse
+
+def reduce_sample_rate(audio, factor=4):
+    return np.repeat(audio[::factor, :], factor, 0)
+
+def reduce_bit_depth(audio, bitdepth=8):
+    factor = 2**(bitdepth-1)
+    return np.round(audio*factor)/factor    # 1 multiply by factor, 2 round, 3 divide by factor 
+
+# Define program arguments
+parser = argparse.ArgumentParser(
+        prog='BitCrusherYves',
+        description='Simple bitcrusher',
+        epilog='Send file, down sample fraction and desired bitrate to get a bitcrushed version of the audio')
+
+parser.add_argument('filename', help='input file, stereo .wav file')
+parser.add_argument('-s', default=4,help='-s down sample fraction', type=int)
+parser.add_argument('-b', default=8, help='-b desired bit depth', type=int)
+parser.add_argument('-o', default='crushed_audio.wav', help='-o optional output file name')
+
+# get arguments
+args = parser.parse_args()
+data, sr = sf.read(args.filename)
+downsample = args.s
+bitdepth = args.b
+output_file = args.o
+
+# do signal processing
+crushed_data = reduce_sample_rate(data, downsample)
+bit_crushed_data = reduce_bit_depth(crushed_data, bitdepth)
+
+# save output file
+sf.write(output_file, bit_crushed_data, sr)
+
+
+
+
+
